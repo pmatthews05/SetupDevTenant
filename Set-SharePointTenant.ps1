c119ad5993949cdc75dfaa7794185f5a9293687a
Paul Matthews <paul.matthews@beis.gov.uk>
2019-04-26 16:47:18 +0100

    Updated Tenant settings and Order of settings.

--- Set-SharePointTenant.ps1
+++ Set-SharePointTenant.ps1
@@ -21,63 +21,70 @@ $Parameters = Get-Content -Raw -Path $SettingsPath | ConvertFrom-Json
                 
 # Uses PNP
 # Set Tenant settings
-Set-PnPTenant -AllowDownloadingNonWebViewableFiles: $($Parameters.AllowDownloadingNonWebViewableFiles) `
-    -AllowEditing: $($Parameters.AllowEditing) `
-    -ApplyAppEnforcedRestrictionsToAdHocRecipients: $($Parameters.ApplyAppEnforcedRestrictionsToAdHocRecipients) `
+Set-PnPTenant -ExternalServicesEnabled: $($Parameters.ExternalServicesEnabled) `
+    -NoAccessRedirectUrl:$($Parameters.NoAccessRedirectUrl) `
+    -SharingCapability:$($Parameters.SharingCapability) `
+    -DisplayStartASiteOption: $($Parameters.DisplayStartASiteOption) `
+    -StartASiteFormUrl: $($Parameters.StartASiteFormUrl) `
+    -ShowEveryoneClaim: $($Parameters.ShowEveryoneClaim) `
+    -ShowAllUsersClaim: $($Parameters.ShowAllUsersClaim) `
+    -ShowEveryoneExceptExternalUsersClaim: $($Parameters.ShowEveryoneExceptExternalUsersClaim) `
+    -SearchResolveExactEmailOrUPN: $($Parameters.SearchResolveExactEmailOrUPN) `
+    -OfficeClientADALDisabled: $($Parameters.OfficeClientADALDisabled) `
+    -LegacyAuthProtocolsEnabled: $($Parameters.LegacyAuthProtocolsEnabled) `
+    -RequireAcceptingAccountMatchInvitedAccount: $($Parameters.RequireAcceptingAccountMatchInvitedAccount) `
+    -ProvisionSharedWithEveryoneFolder: $($Parameters.ProvisionSharedWithEveryoneFolder) `
+    -UsePersistentCookiesForExplorerView: $($Parameters.UsePersistentCookiesForExplorerView) `
     -BccExternalSharingInvitations: $($Parameters.BccExternalSharingInvitations) `
     -BccExternalSharingInvitationsList: $($Parameters.BccExternalSharingInvitationsList) `
-    -CommentsOnSitePagesDisabled: $($Parameters.CommentsOnSitePagesDisabled) `
-    -ConditionalAccessPolicy: $($Parameters.ConditionalAccessPolicy) `
-    -DefaultLinkPermission: $($Parameters.DefaultLinkPermission) `
-    -DefaultSharingLinkType: $($Parameters.DefaultSharingLinkType) `
-    -DisallowInfectedFileDownload: $($Parameters.DisallowInfectedFileDownload) `
-    -DisplayStartASiteOption: $($Parameters.DisplayStartASiteOption) `
-    -EmailAttestationReAuthDays: $($Parameters.EmailAttestationReAuthDays) `
-    -EmailAttestationRequired: $($Parameters.EmailAttestationRequired) `
-    -EnableGuestSignInAcceleration: $($Parameters.EnableGuestSignInAcceleration) `
-    -ExternalServicesEnabled: $($Parameters.ExternalServicesEnabled) `
-    -FileAnonymousLinkType: $($Parameters.FileAnonymousLinkType) `
-    -FilePickerExternalImageSearchEnabled: $($Parameters.FilePickerExternalImageSearchEnabled) `
-    -FolderAnonymousLinkType: $($Parameters.FolderAnonymousLinkType) `
-    -HideDefaultThemes: $($Parameters.HideDefaultThemes) `
+    -UserVoiceForFeedbackEnabled: $($Parameters.UserVoiceForFeedbackEnabled) `
+    -RequireAnonymousLinksExpireInDays: $($Parameters.RequiredAnonymousLinksExpireInDays) `
+    -SharingAllowedDomainList: $($Parameters.SharingAllowedDomainList) `
+    -SharingBlockedDomainList: $($Parameters.SharingBlockedDomainList) `
+    -SharingDomainRestrictionMode: $($Parameters.SharingDomainRestrictionMode) `
+    -OneDriveStorageQuota: $($Parameters.OneDriveStorageQuota) `
     -IPAddressAllowList: $($Parameters.IPAddressAllowList) `
     -IPAddressEnforcement: $($Parameters.IPAddressEnforcement) `
     -IPAddressWACTokenLifetime: $($Parameters.IPAddressWACTokenLifetime) `
-    -LegacyAuthProtocolsEnabled: $($Parameters.LegacyAuthProtocolsEnabled) `
-    -NoAccessRedirectUrl:$($Parameters.NoAccessRedirectUrl) `
+    -DefaultSharingLinkType: $($Parameters.DefaultSharingLinkType) `
+    -ODBMembersCanShare: $($Parameters.ODBMembersCanShare) `
+    -ODBAccessRequests: $($Parameters.ODBAccessRequests) `
+    -PreventExternalUsersFromResharing: $($Parameters.PreventExternalUsersFromResharing) `
+    -ShowPeoplePickerSuggestionsForGuestUsers: $($Parameters.ShowPeoplePickerSuggestionsForGuestUsers) `
+    -FileAnonymousLinkType: $($Parameters.FileAnonymousLinkType) `
+    -FolderAnonymousLinkType: $($Parameters.FolderAnonymousLinkType) `
+    -NotifyOwnersWhenItemsReshared: $($Parameters.NotifyOwnersWhenItemsReshared) `
+    -NotifyOwnersWhenInvitationsAccepted: $($Parameters.NotifyOwnersWhenInvitationsAccepted) `
     -NotificationsInOneDriveForBusinessEnabled: $($Parameters.NotificationsInOneDriveForBusinessEnabled) `
     -NotificationsInSharePointEnabled: $($Parameters.NotificationsInSharePointEnabled) `
-    -NotifyOwnersWhenInvitationsAccepted: $($Parameters.NotifyOwnersWhenInvitationsAccepted) `
-    -NotifyOwnersWhenItemsReshared: $($Parameters.NotifyOwnersWhenItemsReshared) `
-    -ODBAccessRequests: $($Parameters.ODBAccessRequests) `
-    -ODBMembersCanShare: $($Parameters.ODBMembersCanShare) `
-    -OfficeClientADALDisabled: $($Parameters.OfficeClientADALDisabled) `
-    -OneDriveForGuestsEnabled: $($Parameters.OneDriveForGuestsEnabled) `
-    -OneDriveStorageQuota: $($Parameters.OneDriveStorageQuota) `
-    -OrphanedPersonalSitesRetentionPeriod: $($Parameters.OrphanedPersonalSitesRetentionPeriod) `
     -OwnerAnonymousNotification: $($Parameters.OwnerAnonymousNotification) `
-    -PreventExternalUsersFromResharing: $($Parameters.PreventExternalUsersFromResharing) `
-    -ProvisionSharedWithEveryoneFolder: $($Parameters.ProvisionSharedWithEveryoneFolder) `
-    -PublicCdnAllowedFileTypes:  $($Parameters.PublicCdnAllowedFileTypes) `
-    -PublicCdnEnabled:  $($Parameters.PublicCdnEnabled) `
-    -RequireAcceptingAccountMatchInvitedAccount: $($Parameters.RequireAcceptingAccountMatchInvitedAccount) `
-    -RequireAnonymousLinksExpireInDays: $($Parameters.RequiredAnonymousLinksExpireInDays) `
-    -SearchResolveExactEmailOrUPN: $($Parameters.SearchResolveExactEmailOrUPN) `
-    -SharingAllowedDomainList: $($Parameters.SharingAllowedDomainList) `
-    -SharingBlockedDomainList: $($Parameters.SharingBlockedDomainList) `
-    -SharingCapability:$($Parameters.SharingCapability) `
-    -SharingDomainRestrictionMode: $($Parameters.SharingDomainRestrictionMode) `
-    -ShowAllUsersClaim: $($Parameters.ShowAllUsersClaim) `
-    -ShowEveryoneClaim: $($Parameters.ShowEveryoneClaim) `
-    -ShowEveryoneExceptExternalUsersClaim: $($Parameters.ShowEveryoneExceptExternalUsersClaim) `
-    -ShowPeoplePickerSuggestionsForGuestUsers: $($Parameters.ShowPeoplePickerSuggestionsForGuestUsers) `
-    -SignInAccelerationDomain: $($Parameters.SignInAccelerationDomain) `
+    -CommentsOnSitePagesDisabled: $($Parameters.CommentsOnSitePagesDisabled) `
     -SocialBarOnSitePagesDisabled: $($Parameters.SocialBarOnSitePagesDisabled) `
+    -DisallowInfectedFileDownload: $($Parameters.DisallowInfectedFileDownload) `
+    -DefaultLinkPermission: $($Parameters.DefaultLinkPermission) `
+    -ApplyAppEnforcedRestrictionsToAdHocRecipients: $($Parameters.ApplyAppEnforcedRestrictionsToAdHocRecipients) `
+    -FilePickerExternalImageSearchEnabled: $($Parameters.FilePickerExternalImageSearchEnabled) `
+    -EmailAttestationRequired: $($Parameters.EmailAttestationRequired) `
+    -EmailAttestationReAuthDays: $($Parameters.EmailAttestationReAuthDays) `
+    -HideDefaultThemes: $($Parameters.HideDefaultThemes) `
     -SpecialCharactersStateInFileFolderNames: $($Parameters.SpecialCharactersStateInFileFolderNames) `
-    -StartASiteFormUrl: $($Parameters.StartASiteFormUrl) `
     -UseFindPeopleInPeoplePicker: $($Parameters.UseFindPeopleInPeoplePicker) `
-    -UsePersistentCookiesForExplorerView: $($Parameters.UsePersistentCookiesForExplorerView) `
-    -UserVoiceForFeedbackEnabled: $($Parameters.UserVoiceForFeedbackEnabled) `
+
+if ($Parameters.EnableGuestSignInAcceleration) {
+    Set-PnPTenant  -SignInAccelerationDomain: $($Parameters.SignInAccelerationDomain) `
+        -EnableGuestSignInAcceleration: $($Parameters.EnableGuestSignInAcceleration)
+}
+
+# Needs a confirm.
+Set-PnPTenant -OneDriveForGuestsEnabled: $($Parameters.OneDriveForGuestsEnabled) 
+Set-PnPTenant -OrphanedPersonalSitesRetentionPeriod: $($Parameters.OrphanedPersonalSitesRetentionPeriod) 
+
+# Needs Itune and Azure Directory Premium Subscriptions
+#TODO: Work out how to check if licenses are available before running.
+#Set-PnPTenant -ConditionalAccessPolicy: $($Parameters.ConditionalAccessPolicy) 
+#Set-PnPTenant -AllowDownloadingNonWebViewableFiles: $($Parameters.AllowDownloadingNonWebViewableFiles) 
+#Set-PnPTenant -AllowEditing: $($Parameters.AllowEditing) `
+  
   
 <#This sets 
 */MASTERPAGE
@@ -86,5 +93,14 @@ Set-PnPTenant -AllowDownloadingNonWebViewableFiles: $($Parameters.AllowDownloadi
 #>
 Write-Information -MessageData "Setting Public CDN to enabled = $($Parameters.PublicCdnEnabled)"
 Set-PnPTenantCdnEnabled -CdnType Public -Enable:$Parameters.PublicCdnEnabled
+if($Parameters.PublicCdnEnabled)
+{
+    Set-PnPTenantCdnPolicy -CdnType Public -PolicyType:IncludeFileExtensions -PolicyValue:$($Parameters.PublicCdnAllowedFileTypes) 
+    
+    $Parameters.PublicCdnOrigins | ForEach-Object{
+        Add-PnPTenantCdnOrigin -CdnType Public -OriginUrl $PSItem -ErrorAction:SilentlyContinue
+    }
+    
+}
 
 Write-Host "Finished setting tenant settings at $(Get-Date)"
